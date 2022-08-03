e489ee823c2d18c930209471af3e507aba64525e
EKamenyuk <e.kamenyuk@mail.ru>
2022-08-03 22:34:48 +0700

    форматирование кода

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,7 +2,7 @@ fun main() {
     val likes = 31  // изменяемая величина. количество лайков
     val result = likes % 10
     if (2 <= likes && likes <= 20) println("Понравилось " + likes + " человекам")
-    else if(result == 1) println("Понравилось " + likes + " человеку")
+    else if (result == 1) println("Понравилось " + likes + " человеку")
     else println("Понравилось " + likes + " людям")
 }
 

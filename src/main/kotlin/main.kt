3aee2b711276977ceb0f00214bbc233266f462fa
EKamenyuk <e.kamenyuk@mail.ru>
2022-08-03 22:30:24 +0700

    Внесены дополнения

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,8 @@
 fun main() {
-    val likes = 1011  // изменяемая величина
+    val likes = 31  // изменяемая величина. количество лайков
     val result = likes % 10
-    if (result == 1) println("Понравилось " + likes + " человеку")
+    if (2 <= likes && likes <= 20) println("Понравилось " + likes + " человекам")
+    else if(result == 1) println("Понравилось " + likes + " человеку")
     else println("Понравилось " + likes + " людям")
 }
 
